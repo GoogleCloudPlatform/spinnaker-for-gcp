3a21eafff6812614ae860cb47e8f96efbdd1a678
Matt Duftler <duftler@google.com>
2019-07-03 14:17:18 -0400

    Bump Spinnaker to 1.14.8 and hal to 1.21.2. (#51)

--- scripts/install/setup_properties.sh
+++ scripts/install/setup_properties.sh
@@ -88,8 +88,8 @@ else
 export PROJECT_ID=$PROJECT_ID
 export DEPLOYMENT_NAME=${DEPLOYMENT_NAME:-$NEW_DEPLOYMENT_NAME}
 
-export SPINNAKER_VERSION=1.14.7
-export HALYARD_VERSION=1.20.2
+export SPINNAKER_VERSION=1.14.8
+export HALYARD_VERSION=1.21.2
 
 # The specified network must exist, and it must not be a legacy network.
 # More info on legacy networks can be found here: https://cloud.google.com/vpc/docs/legacy
