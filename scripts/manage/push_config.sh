5b2a34e07626bf812b4e0a275e6e24bc04ae817d
Louis Jimenez <louisjimenez@google.com>
2019-08-07 09:40:45 -0400

    Louis/check git config (#94)
    Check that git identity is configured when pushing config.

--- scripts/manage/push_config.sh
+++ scripts/manage/push_config.sh
@@ -4,6 +4,19 @@ bold() {
   echo ". $(tput bold)" "$*" "$(tput sgr0)";
 }
 
+GIT_USERNAME=$(git config --global --get user.name)
+GIT_EMAIL=$(git config --global --get user.email)
+
+if [ -z "$GIT_USERNAME" ]; then
+  bold "Your Git account username is not set. Run 'git config --global user.name \"Your Name\"' and try again."
+  exit 1
+fi
+
+if [ -z "$GIT_EMAIL" ]; then
+  bold "Your Git account email is not set. Run 'git config --global user.email \"you@example.com\"' and try again."
+  exit 1
+fi
+
 source ~/spinnaker-for-gcp/scripts/install/properties
 
 # TODO(duftler): Add check to ensure that we are not overriding with older or empty config.
