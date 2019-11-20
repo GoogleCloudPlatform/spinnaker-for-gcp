4ae7a1ff87b74c31510bc54ed314f7dae96772eb
Louis Jimenez <louisjimenez@google.com>
2019-11-20 15:01:41 -0500

    Indicate IAP config is not affected by restore

--- scripts/manage/restore_backup_to_cloud_shell.sh
+++ scripts/manage/restore_backup_to_cloud_shell.sh
@@ -117,5 +117,6 @@ rm -rf $TEMP_DIR
 
 bold "Configuration applied. To diff this config with what was last deployed, go to:"
 bold "https://source.cloud.google.com/$PROJECT_ID/$EXISTING_CSR_REPO/+/$GIT_HASH...master"
+bold "Note: Any pre-existing configuration for secure access to Spinnaker via IAP has not been modified."
 bold "To apply the halyard config changes to the cluster, run:"
 bold "~/spinnaker-for-gcp/scripts/manage/push_and_apply.sh"
