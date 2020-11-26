d31c779090eaeb2a60030fddbf3b28aa743bfd7d
Jeremy Berg <jerberg424@gmail.com>
2020-11-25 19:57:01 -0600

    sanity

--- scripts/manage/check_project_mismatch.sh
+++ scripts/manage/check_project_mismatch.sh
@@ -7,7 +7,7 @@ source $PARENT_DIR/scripts/manage/service_utils.sh
 [ -z "$PROPERTIES_FILE" ] && PROPERTIES_FILE="$PARENT_DIR/scripts/install/properties"
 
 source "$PROPERTIES_FILE"
-echo $GCLOUD_PROJECT_ID
+echo "$GCLOUD_PROJECT_ID This is fun"
 gcloud info --format='value(config.project)'
 GCLOUD_PROJECT_ID=$(gcloud info --format='value(config.project)')
 echo GCLOUD_PROJECT_ID
