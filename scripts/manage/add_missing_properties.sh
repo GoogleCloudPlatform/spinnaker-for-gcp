344bea94fa19e3dfb67d26c6289ad2cd9da4d312
Jakub Stransky <stransky.ja@gmail.com>
2020-12-27 14:30:35 +0100

    make spin cli configuration optional

--- scripts/manage/add_missing_properties.sh
+++ scripts/manage/add_missing_properties.sh
@@ -4,9 +4,9 @@ bold() {
   echo ". $(tput bold)" "$*" "$(tput sgr0)";
 }
 
-[ -z "$PARENT_DIR" ] && PARENT_DIR=$(dirname $(realpath $0) | rev | cut -d '/' -f 4- | rev)
+#[ -z "$PARENT_DIR" ] && PARENT_DIR=$(dirname $(realpath $0) | rev | cut -d '/' -f 4- | rev)
 
-PROPERTIES_FILE=$PARENT_DIR/spinnaker-for-gcp/scripts/install/properties
+PROPERTIES_FILE=./../install/properties
 
 add_property_if_missing() {
   if [ -z "$(grep "export $1=" $PROPERTIES_FILE)" ]; then
@@ -28,3 +28,5 @@ add_property_if_missing CONFIG_CSR_REPO "$CSR_PROPERTY_DECLARATION"
 add_property_if_missing NETWORK_PROJECT "export NETWORK_PROJECT=\$PROJECT_ID"
 add_property_if_missing NETWORK_REFERENCE "export NETWORK_REFERENCE=projects/\$NETWORK_PROJECT/global/networks/\$NETWORK"
 add_property_if_missing SUBNET_REFERENCE "export SUBNET_REFERENCE=projects/\$NETWORK_PROJECT/regions/\$REGION/subnetworks/\$SUBNET"
+
+add_property_if_missing CONFIGURE_SPIN_CLI "export CONFIGURE_SPIN_CLI=YES"
